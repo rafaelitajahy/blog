e5398956128b3ef780524247f040a350dcbf719a
rafaelitajahy <rafaelitajahy@gmail.com>
2019-12-19 17:19:25 -0300

    Add Sobre

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Rafael Itajahy</h1>
+    <h2>Especialista em Marketing Digital</h2>
+    <p>Criado do MyBlog, entusiasta do trabalho digital</p>
+  </div>
+)
+
+export default Profile
