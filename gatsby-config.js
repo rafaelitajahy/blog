b912120fd3d7189af7a519ef03020c744bca7ea5
rafaelitajahy <rafaelitajahy@gmail.com>
2019-12-19 11:30:02 -0300

    Preparando o projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Meu Blog`,
+    description: `Um blog simples e poderoso para marketing digital`,
+    author: `@myblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
